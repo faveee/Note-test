3a6b2efe0980944a55de1da7f201a974fb96c696
faveee <abatangift11@gmail.com>
2023-01-26 12:10:00 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+const [open, setOpen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='menu-container'>
+        <div className='kode'>
+        <button className='trigger' onClick={() => {setOpen(!open)}}>
+          {open ? 'Close' : 'Notes'}
+</button>
+</div>
+
+        <div className={`dropdown-page ${open? 'active' : 'inactive'}`} >
+        <h3 className='dropdown-item'>Notes</h3>
+        </div>
+    </div>
     </div>
   );
 }
